cf522f833c3f9d7300ba8cc95ad9712809933457
Alex Naidis <alex.naidis@linux.com>
2017-03-17 15:56:01 +0100

    Keypress Boost: Block multiple Keypress boosts
    We need to cover the case where a new Keypress boost
    gets scheduled while the previous Keypress boost is still
    running.
    
    As first step, this patch blocks new Keypress from coming
    up until the previous Keypress boost finished.
    
    Also we force a 50ms delay between the old Keypress boost
    and the new Keypress boost.
    
    Change-Id: If1192e282b2e0deffc90d2339587740f1bdde5ff
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -427,6 +427,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean lIsPerfBoostEnabled;
     private int[] mBoostParamValWeak;
     private int[] mBoostParamValStrong;
+    private boolean mKeypressBoostBlocked;
 
     // FIXME This state is shared between the input reader and handler thread.
     // Technically it's broken and buggy but it has been like this for many years
@@ -804,6 +805,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final int MSG_REQUEST_TRANSIENT_BARS_ARG_STATUS = 0;
     private static final int MSG_REQUEST_TRANSIENT_BARS_ARG_NAVIGATION = 1;
 
+    private static final int MSG_DISPATCH_KEYPRESS_BOOST_UNBLOCK = 100;
+
     private boolean mClearedBecauseOfForceShow;
     private boolean mTopWindowIsKeyguard;
 
@@ -890,6 +893,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     dispatchMediaKeyWithWakeLockToAudioService(
                             KeyEvent.changeAction(event, KeyEvent.ACTION_UP));
                     mVolumeMusicControlActive = true;
+                    break;
+                }
+                case MSG_DISPATCH_KEYPRESS_BOOST_UNBLOCK:
+                    mKeypressBoostBlocked = false;
                     break;
                 }
             }
@@ -4146,6 +4153,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mBoostDuration != 0) {
             Slog.i(TAG, "Dispatching Keypress boost for " + mBoostDuration + " ms.");
             mPerf.perfLockAcquire(mBoostDuration, mBoostParamVal);
+
+            // Block Keypress boost
+            mKeypressBoostBlocked = true;
+
+            // Calculate unblock time and dispatch delayed unblock MSG
+            int mBoostBlockTime = mBoostDuration + 50/*ms*/;
+            mHandler.sendEmptyMessageDelayed(MSG_DISPATCH_KEYPRESS_BOOST_UNBLOCK, mBoostBlockTime);
+
         }
     }
 
@@ -6146,7 +6161,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         // Intercept the Keypress event for Keypress boost
-        if (lIsPerfBoostEnabled) {
+        if (lIsPerfBoostEnabled && !mKeypressBoostBlocked) {
             dispatchKeypressBoost(keyCode);
         }
 
@@ -8831,6 +8846,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 }
+
 
 
 
